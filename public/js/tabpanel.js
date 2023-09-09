d7817b24fa36a1028a2b6670024e18a3d5a57f03
yangdehua1981 <26110395@qq.com>
2023-09-09 22:24:19 +0800

    tab样式修改

--- public/js/tabpanel.js
+++ public/js/tabpanel.js
@@ -16,38 +16,65 @@ export class Tabpage {
         if (this.owner && this.owner.curtab && this.owner.curtab != this) {
             let curtab = this.owner.curtab;
             curtab.section.style.display = "none";
-            curtab.tab.style.backgroundColor = "";
+            let curstyle = curtab.tab.style;
+            curstyle.backgroundColor = "";
+            curstyle.borderBottom = '0px';
+            curstyle.borderLeft = '0px';
+            curstyle.borderRight = '0px';
+            curstyle.borderTop = '0px';
         }
         this.owner.curtab = this;
         this.section.style.display = "block";
-        this.tab.style.backgroundColor = "rgba(56,78,32,0.8)";
+        let style = this.tab.style;
+        if (this.owner.direct == TabPanel.Dir.TOP)
+            this.owner.topstyle(style);
+        else if (this.owner.direct == TabPanel.Dir.BOTTOM)
+            this.owner.bottomstyle(style);
+        else if (this.owner.direct == TabPanel.Dir.LEFT)
+            this.owner.leftstyle(style);
+        else if (this.owner.direct == TabPanel.Dir.RIGHT)
+            this.owner.rightstyle(style);
+
     }
     #createtab() {
         this.tab = document.createElement("li");
         let tab = this.tab;
         tab.className = "tab";
-
-        tab.style.paddingLeft = "4px";
-        tab.style.paddingRight = "4px";
-        tab.style.overflow = 'hidden';
-        tab.style.textOverflow = 'ellipsis';
-        tab.style.whiteSpace = 'nowrap';
+        let style = tab.style;
+        style.overflow = 'hidden';
+        style.textOverflow = 'ellipsis';
+        style.whiteSpace = 'nowrap';
         tab.innerHTML = this.title;
         if (this.owner.direct == TabPanel.Dir.TOP || this.owner.direct == TabPanel.Dir.BOTTOM) {
-
-            tab.style.display = "inline-block";
-            tab.style.width = "40px";
+            if (this.owner.direct == TabPanel.Dir.TOP) {
+                style.borderTopLeftRadius = "5px";
+                style.borderTopRightRadius = "5px";
+            }
+            else if (this.owner.direct == TabPanel.Dir.BOTTOM) {
+                style.borderBottomLeftRadius = "5px";
+                style.borderBottomRightRadius = "5px";
+            }
+            style.display = "inline-block";
+            style.width = "40px";
+            style.height = '20px';
         }
         else if (this.owner.direct == TabPanel.Dir.LEFT || this.owner.direct == TabPanel.Dir.RIGHT) {
-            tab.style.width = "40px";
-            tab.style.height = '40px';
-            tab.style.display = "flex";
-            tab.style.alignItems = "center";
-            tab.style.justifyContent = "center";
-            //tab.innerHTML = `<span style="text-overflow:ellipsis;white-space:nowrap;">${this.title}</span>`;
+            style.width = "40px";
+            style.height = '40px';
+            style.display = "flex";
+            style.alignItems = "center";
+            style.justifyContent = "center";
+            if (this.owner.direct == TabPanel.Dir.LEFT) {
+                style.borderTopLeftRadius = "5px";
+                style.borderBottomLeftRadius = "5px";
+            }
+            else if (this.owner.direct == TabPanel.Dir.RIGHT) {
+                style.borderTopRightRadius = "5px";
+                style.borderBottomRightRadius = "5px";
+            }
         }
-        tab.style.textAlign = "center";
-        tab.style.caretColor = "transparent";
+        style.textAlign = "center";
+        style.caretColor = "transparent";
         tab.addEventListener("mouseenter", this.#onmouseenter.bind(this));
         tab.addEventListener("mouseleave", this.#onmouseleave.bind(this));
         tab.addEventListener("click", this.#onmouseclick.bind(this));
@@ -55,12 +82,13 @@ export class Tabpage {
     #createpage() {
         this.section = document.createElement("section");
         let sec = this.section;
-        sec.style.position = "absolute";
-        sec.style.width = "100%";
-        sec.style.top = '0';
-        sec.style.bottom = '0';
-        sec.style.display = "none";
-        sec.style.backgroundColor = "#ddeeff";
+        let style = sec.style;
+        style.position = "absolute";
+        style.width = "100%";
+        style.top = '0';
+        style.bottom = '0';
+        style.display = "none";
+        style.backgroundColor = "#ddeeff";
         sec.innerHTML = `${this.index}`;
     }
     Create() {
@@ -104,27 +132,89 @@ export class TabPanel {
     }
     #createmainul() {
         this.mainul = document.createElement("ul");
-        this.mainul.style.listStyleType = "none";
-        this.mainul.style.padding = "0px";
-        this.mainul.style.margin = "0px";
-        if (this.direct === TabPanel.Dir.UP) {
-            //this.mainul.style.display = "flex";
+        let style = this.mainul.style;
+        style.listStyleType = "none";
+        style.padding = "0px";
+        style.margin = "0px";
+
+        if (this.direct === TabPanel.Dir.TOP) {
+            style.display = "flex";
+            style.borderBottom = "1px solid #000000";
+            style.height = '20px';
         }
-        else if (this.direct === TabPanel.Dir.DOWN) {
-            //this.mainul.style.display = "flex";
+        else if (this.direct === TabPanel.Dir.BOTTOM) {
+            style.borderTop = "1px solid #000000";
+            style.display = "flex";
+            style.height = '20px';
+        }
+        else if (this.direct === TabPanel.Dir.LEFT) {
+            style.borderRight = "1px solid #000000";
+            style.width = '40px';
+        }
+        else if (this.direct === TabPanel.Dir.RIGHT) {
+            style.borderLeft = "1px solid #000000";
+            style.width = '40px';
         }
     }
     #createcontent() {
         this.content = document.createElement("div");
         this.content.className = "panelcontainer";
-        this.content.style.margin = "0px";
-        this.content.style.height = "100%";
-        this.content.style.backgroundColor = "#FFffff";
-        this.content.style.border = "1px solid #000000";
+        let style = this.content.style;
+        style.margin = "0px";
+        style.height = "100%";
+        style.backgroundColor = "#FFffff";
+        if (this.direct === TabPanel.Dir.TOP) {
+            style.borderLeft = "1px solid #000000";
+            style.borderRight = "1px solid #000000";
+            style.borderBottom = "1px solid #000000";
+        }
+        else if (this.direct === TabPanel.Dir.BOTTOM) {
+            style.borderLeft = "1px solid #000000";
+            style.borderRight = "1px solid #000000";
+            style.borderTop = "1px solid #000000";
+        }
+        else if (this.direct === TabPanel.Dir.LEFT) {
+            style.borderRight = "1px solid #000000";
+            style.borderBottom = "1px solid #000000";
+            style.borderTop = "1px solid #000000";
+        }
+        else if (this.direct === TabPanel.Dir.RIGHT) {
+            style.borderLeft = "1px solid #000000";
+            style.borderBottom = "1px solid #000000";
+            style.borderTop = "1px solid #000000";
+        }
 
-        this.content.style.flexGrow = "1";
-        this.content.style.overflowY = "auto";
-        this.content.style.position = 'relative';
+        style.flexGrow = "1";
+        style.overflowY = "auto";
+        style.position = 'relative';
+    }
+    topstyle(style) {
+        style.backgroundColor = "rgba(56,78,32,1)";
+        style.borderBottom = '1px solid rgba(56,78,32,1)';
+        style.borderLeft = '1px solid #000000';
+        style.borderRight = '1px solid #000000';
+        style.borderTop = '1px solid #000000';
+    }
+    bottomstyle(style) {
+        style.backgroundColor = "rgba(56,78,32,1)";
+        style.borderTop = '1px solid rgba(56,78,32,1)';
+        style.borderLeft = '1px solid #000000';
+        style.borderRight = '1px solid #000000';
+        style.borderBottom = '1px solid #000000';
+    }
+    leftstyle(style) {
+        style.backgroundColor = "rgba(56,78,32,1)";
+        style.borderRight = '1px solid rgba(56,78,32,1)';
+        style.borderLeft = '1px solid #000000';
+        style.borderTop = '1px solid #000000';
+        style.borderBottom = '1px solid #000000';
+    }
+    rightstyle(style) {
+        style.backgroundColor = "rgba(56,78,32,1)";
+        style.borderLeft = '1px solid rgba(56,78,32,1)';
+        style.borderRight = '1px solid #000000';
+        style.borderTop = '1px solid #000000';
+        style.borderBottom = '1px solid #000000';
     }
     Create() {
         let container = document.getElementById(this.parentid);
@@ -142,7 +232,15 @@ export class TabPanel {
         this.content.appendChild(tabpage.section);
         if (this.tabcount == 0) {
             tabpage.section.style.display = "block";
-            tabpage.tab.style.backgroundColor = "rgba(56,78,32,0.8)";
+            let style = tabpage.tab.style;
+            if (this.direct === TabPanel.Dir.TOP)
+                this.topstyle(style);
+            else if (this.direct === TabPanel.Dir.BOTTOM)
+                this.bottomstyle(style);
+            else if (this.direct === TabPanel.Dir.LEFT)
+                this.leftstyle(style);
+            else if (this.direct === TabPanel.Dir.RIGHT)
+                this.rightstyle(style);
             this.curtab = tabpage;
         }
         this.tabcount++;
